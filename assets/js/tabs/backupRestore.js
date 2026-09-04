2f4d60a56071b37e77d009008e8dec16db4f17a7
ruditocima <rudi.tcm@gmail.com>
2026-09-04 20:10:42 +0700

    Delete assets directory

--- assets/js/tabs/backupRestore.js
+++
@@ -1,3 +0,0 @@
-export function backupRestoreTab() {
-    return { init() {}, handleImportFile(event) { this.$parent.handleImportFile(event); } };
-}
--- assets/js/tabs/transaksiApproval.js
+++
@@ -1,7 +0,0 @@
-export function transaksiApprovalTab() {
-    return {
-        init() {},
-        approve(trx) { this.$parent.approveTransaction(trx); },
-        reject(trx) { this.$parent.rejectTransaction(trx); }
-    };
-}
