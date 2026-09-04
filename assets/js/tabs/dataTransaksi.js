cb9e57a4cc3f552a3483fb985e81b3cb9ac2ca6e
ruditocima <rudi.tcm@gmail.com>
2026-09-04 17:50:54 +0700

    Update dataTransaksi.js

--- assets/js/tabs/dataTransaksi.js
+++ assets/js/tabs/dataTransaksi.js
@@ -6,7 +6,8 @@ export function dataTransaksiTab() {
         init() { this.page = 1; },
         get filteredTransactionList() {
             let q = this.searchTransaction.toLowerCase();
-            return this.$parent.transactionList.filter(t => t.noDocument.toLowerCase().includes(q) || t.noTodo.toLowerCase().includes(q));
+            const txList = this.$store?.wms?.transactionList || [];
+            return txList.filter(t => t.noDocument.toLowerCase().includes(q) || (t.noTodo && t.noTodo.toLowerCase().includes(q)));
         },
         get totalPages() { return totalPages(this.filteredTransactionList, this.perPage); },
         get paginatedTransactionList() { return paginate(this.filteredTransactionList, this.page, this.perPage); }
