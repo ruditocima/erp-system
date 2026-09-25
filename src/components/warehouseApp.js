98341ce3ca2c552b76b5238aff29311f9fbe805c
ruditocima <rudi.tcm@gmail.com>
2026-09-25 22:20:49 +0700

    Update warehouseApp.js

--- src/components/warehouseApp.js
+++ src/components/warehouseApp.js
@@ -13,7 +13,7 @@ function safeLoadStorage(key, fallback) {
 export default function warehouseApp() {
     return {
         // Properti URL Web App Google Apps Script
-        googleScriptUrl: 'https://script.google.com/macros/s/AKfycby3aioGdSmMvxUOB65uYRMFowwAGRKZsPO88Kgiqsjs/dev',
+        googleScriptUrl: 'https://script.google.com/macros/s/AKfycbwdT2yJe7z7zC9on5gXS4BwDp5keJzgubNE0ypOPtupP5Pwh-74VIwrVGcBWOL4vFYz5w/exec',
 
         isLoggedIn: localStorage.getItem('vortex_logged_in') === 'true',
         currentUser: localStorage.getItem('vortex_user') || 'Admin',
